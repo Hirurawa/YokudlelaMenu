281e0ee0364cec56471ba0c4c64789a7bbcc6039
Siteri Dávid <siteridavid@gmail.com>
2021-10-01 14:05:06 +0200

    Add files via upload

--- YokudlelaMenu/src/main/java/datamodel/Food.java
+++ YokudlelaMenu/src/main/java/datamodel/Food.java
@@ -4,21 +4,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @EqualsAndHashCode()
 @NoArgsConstructor
-public class Menu {
+public class Food {
 
     private String name;
-    private List<Food> items = new ArrayList<>();
     private int quantity;
-    private int price;
-
-    public Menu(String name){
-        this.name = name;
-    }
 
 }
