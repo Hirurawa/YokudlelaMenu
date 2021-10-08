2ec0d4094f3450efb071215d7f8b03944c587f70
Siteri Dávid <siteridavid@gmail.com>
2021-10-08 13:01:08 +0200

    Add files via upload

---
+++ YokudlelaMenu/src/main/java/yokudlela/menu/datamodel/Food.java
@@ -0,0 +1,15 @@
+package yokudlela.menu.datamodel;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode()
+@NoArgsConstructor
+public class Food {
+
+    private String name;
+    private int quantity;
+
+}
